ed5f0bd5fefe3eed2fd58fa17fe576bd6425c571
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2021-01-31 06:21:11 +0100

    todo

--- image-analysis-function/src/main/java/dev/jozefowicz/companydisc/function/handler/ImageAnalysisHandler.java
+++
@@ -1,7 +0,0 @@
-package dev.jozefowicz.companydisc.function.handler;
-
-import com.amazonaws.services.lambda.runtime.events.S3Event;
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class ImageAnalysisHandler extends SpringBootRequestHandler<S3Event, Void> {
-}
